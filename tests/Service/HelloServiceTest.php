77164aa5b4f98e25b85bb76c11c9797feadd964e
Jasmin0101 <zasminalave@gmail.com>
2025-04-12 17:49:01 +1000

    add phpunit and sample  test

---
+++ tests/Service/HelloServiceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Hello;
+use App\Repository\HelloRepository;
+use App\Service\HelloService;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+
+class HelloServiceTest extends TestCase
+{
+    private HelloRepository|MockObject $helloRepository;
+    private HelloService $helloService;
+    protected function setUp(): void
+    {
+        $this->helloRepository = $this->createMock(HelloRepository::class);
+        $this->helloService = new HelloService(
+            $this->helloRepository
+        );
+    }
+
+    /**
+     * @dataProvider provideLuckyNumber
+     */
+    public function testGenerateLuckyNumber(string $expectedLuckyNumber): void
+    {
+        
+        $helloObject = $this->createMock(Hello::class);
+        $helloObject
+            ->expects($this->once())
+            ->method('getLuckyNumber')
+            ->willReturn($expectedLuckyNumber)
+        ;
+
+
+        $this->helloRepository
+            ->expects($this->once())
+            ->method('createLuckyNumber')
+            ->willReturn($helloObject)
+        ;
+
+
+        $this->assertEquals(
+            $expectedLuckyNumber,
+            $this->helloService->generateLuckyNumber()
+        );
+    }
+
+    
+    public static function provideLuckyNumber(): array
+    {
+        return [
+            'Один' => ['1'],
+            'Два' => ['2'],
+            'Три' => ['3'],
+        
+        ];
+    }
+}
